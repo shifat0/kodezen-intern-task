6e550cc7d9a18b1a58c0b2f4451790b1ee6ccc41
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-15 23:55:32 +0600

    main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./main.css";
 import Navbar from "../Navbar/Navbar";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Main = () => {
   return (
     <div className="main-container">
       <Navbar />
+      <div className="main-items">
+        <h1>React Task</h1>
+        <textarea defaultValue="Add your heading task here" />
+        <div className="main-upload">
+          <div className="main-upload-btns">
+            <button>+</button>
+            <label>
+              <input type="file" />
+              <button>
+                <FontAwesomeIcon icon={faFolder} className="main-folder-icon" />
+              </button>
+            </label>
+          </div>
+          <span>Drag widget here</span>
+        </div>
+      </div>
     </div>
   );
 };
